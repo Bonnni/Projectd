42c16c1c6bc618a9ff00c3dd453569335acbbf24
Вячеслав Чижевский <12-slava@mail.ru>
2021-03-19 18:53:22 +0500

    1

--- DataLayer/SampleData.cs
+++ DataLayer/SampleData.cs
@@ -17,16 +17,16 @@ namespace DataLayer
                     });
                 context.SaveChanges();
                 context.Players.AddRange(new List<Player> {
-                    new Player {FirstName = "Татьяна", LastName = "Александрова", TeamId = 1, Gender = "Женский", BirthDay = "03.03.2021", Country = "Россия"},
-                    new Player {FirstName = "Александр ", LastName = "Белозерцев",TeamId = 1, Gender = "Мужской", BirthDay = "03.03.2021", Country = "США"},
-                    new Player {FirstName = "Елена", LastName = "Богородицкая", TeamId = 1, Gender = "Женский", BirthDay = "03.03.2021", Country = "Италия"},
-                    new Player {FirstName = "Елена", LastName = "Великанова", TeamId = 2, Gender = "Женский", BirthDay = "03.03.2021", Country = "Италия"},
-                    new Player {FirstName = "Алла", LastName = "Власова", TeamId = 2, Gender = "Женский", BirthDay = "03.03.2021", Country = "США"},
-                    new Player {FirstName = "Павел", LastName = "Гребенников ", TeamId = 2, Gender = "Мужской", BirthDay = "03.03.2021", Country = "Россия"},
-                    new Player {FirstName = "Вадим", LastName = "Гильмутдинов", TeamId = 3, Gender = "Мужской", BirthDay = "03.03.2021", Country = "Италия"},
-                    new Player {FirstName = "Даниил", LastName = "Насыров", TeamId = 3, Gender = "Мужской", BirthDay = "03.03.2021", Country = "США"},
-                    new Player {FirstName = "Николай", LastName = "Сергеев", TeamId = 3, Gender = "Мужской", BirthDay = "03.03.2021", Country = "США"},
-                    new Player {FirstName = "Куперт", LastName = "Захаров", TeamId = 3, Gender = "Мужской", BirthDay = "03.03.2021", Country = "Россия"}
+                    new Player {FirstName = "Татьяна", LastName = "Александрова", TeamId = 1, Gender = "Женский", BirthDay = "1997-03-03", Country = "Россия"},
+                    new Player {FirstName = "Александр ", LastName = "Белозерцев",TeamId = 1, Gender = "Мужской", BirthDay = "1998-05-08", Country = "США"},
+                    new Player {FirstName = "Елена", LastName = "Богородицкая", TeamId = 1, Gender = "Женский", BirthDay = "1997-12-07", Country = "Италия"},
+                    new Player {FirstName = "Елена", LastName = "Великанова", TeamId = 2, Gender = "Женский", BirthDay = "1996-06-10", Country = "Италия"},
+                    new Player {FirstName = "Алла", LastName = "Власова", TeamId = 2, Gender = "Женский", BirthDay = "1996-08-03", Country = "США"},
+                    new Player {FirstName = "Павел", LastName = "Гребенников ", TeamId = 2, Gender = "Мужской", BirthDay = "1997-01-02", Country = "Россия"},
+                    new Player {FirstName = "Вадим", LastName = "Гильмутдинов", TeamId = 3, Gender = "Мужской", BirthDay = "1995-02-13", Country = "Италия"},
+                    new Player {FirstName = "Даниил", LastName = "Насыров", TeamId = 3, Gender = "Мужской", BirthDay = "1995-11-15", Country = "США"},
+                    new Player {FirstName = "Николай", LastName = "Сергеев", TeamId = 3, Gender = "Мужской", BirthDay = "1997-09-20", Country = "США"},
+                    new Player {FirstName = "Куперт", LastName = "Захаров", TeamId = 3, Gender = "Мужской", BirthDay = "2000-02-27", Country = "Россия"}
                 });
                 context.SaveChanges();
             }
--- Project/wwwroot/js/site.js
+++ Project/wwwroot/js/site.js
@@ -2,12 +2,19 @@
 const viewBagTeam = JSON.parse(team.replace(/&quot;/g, '"'));
 const country = ["США", "Россия", "Италия"];
 
+function getGender(value) {
+    if (value === "Мужской") {
+        value = "Женский";
+    } else { value = "Мужское" }
+    return value;
+}
 
-$('.btn-change').click(function () {
-    player = $(this).parent('td').siblings('td').map(function (i, td) {
+
+$('.btn-change').click(function() {
+    player = $(this).parent('td').siblings('td').map(function(i, td) {
         return $(td).text();
     });
-    let optionTeam = `<option value="${player[5]}" selected>${player[5]}</option>`;
+    let optionTeam = `<option selected>${player[5]}</option>`;
     let optionCountry = `<option selected>${player[6]}</option>`;
 
 
@@ -23,38 +30,38 @@ $('.btn-change').click(function () {
     };
 
 
-    const addTable = `<form id="formAction", method="post"><div class="form-group"><input name="Id" type="hidden" value="${player[0]
-        }" /><label class="col-form-label col-6">Имя</label><input name="FirstName" type="text" class="form-control col-12" placeholder="Имя" value="${
-        player[1]
-        }" /><label class="col-form-label col-6">Фамилия</label><input name="LastName" type="text" class="form-control col-12" placeholder="Введите фамилию" value="${
-        player[2]
-        }" /><label class="col-form-label col-6">Пол</label><input name="Gender" type="text" class="form-control col-12" placeholder="Пол" value="${
-        player[3]
-        }" /><label class="col-form-label col-6">Дата рождения</label><input name="BirthDay" type="text" class="form-control col-12" placeholder="Дата рождения" value="${
-        player[4]
-        }" /><label class="col-form-label col-6">Команда</label>
-        <select name="Team" type="text" class="form-control col-12" placeholder="Введите команду">
-            ${optionTeam}
-        </select>
-<label class="col-form-label col-6">Страна</label>
-            <select name="Country" type="text" class="form-control col-12" placeholder="Введите страну">
-                    ${optionCountry}
-        </select></div><div class="row"><div class="form-group col-8"><input type="submit" id="btnSub" value="Изменить" class="btn btn-primary"/></div><div class="form-group col-4"><button id="btnClose" class="btn btn-danger" style:"margin-left: 80%">Закрыть</button></div><div></form>`;
 
+    const addTable = `<form id="formAction" action="Home/Edit" method="POST">
+                    <div class="form-group"><input name="Id" type="hidden" value="${player[0]}" />
+                        <label class="col-form-label col-6">Имя</label>
+                        <input name="FirstName" type="text" class="form-control col-12" placeholder="Имя" value="${player[1]}" />
+                        <label class="col-form-label col-6">Фамилия</label>
+                        <input name="LastName" type="text" class="form-control col-12" placeholder="Введите фамилию" value="${player[2]}" />
+                        <label class="col-form-label col-6">Пол</label>
+                        <select name="Gender" type="text" class="form-control col-12" placeholder="Пол">
+                            <option value="${player[3]}" selected>${player[3]}</option>
+                            <option value="${getGender(player[3])}">${getGender(player[3])}</option>
+                        </select>
+                        <label class="col-form-label col-6">Дата рождения</label>
+                        <input name="BirthDay" type="date" class="form-control col-12" placeholder="Дата рождения" value="${player[4]}" />
+                        <label class="col-form-label col-6">Команда</label>
+                        <select name="TeamId" type="text" class="form-control col-12" placeholder="Введите команду">${optionTeam}</select>
+                        <label class="col-form-label col-6">Страна</label>
+                        <select name="Country" type="text" class="form-control col-12" placeholder="Введите страну">${optionCountry}</select>
+                    </div>
+                            <div class="row">
+                                <div class="form-group col-8">
+                                    <input type="submit" id="btnSub" value="Изменить" class="btn btn-primary"/>
+                                </div>
+                                <div class="form-group col-4">
+                                    <input type="button" id="btnClose" class="btn btn-danger" value="Закрыть">
+                                </div>
+                            </div>
+                    </form>`;
 
     $('#form-change').html(addTable).addClass("col-sm").css({ 'margin-top': '3%' });
 });
 
-$("#formAction").submit(function (e) {
-
-    let url = "https://localhost:44348/Home/Edit" + player[0];
-
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: $("#formAction").serialize()
-    });
-    e.preventDefault();
+$('#btnClose').click(function() {
+    $('#form-change').empty();
 });
-
-
